4828c633d308052c0c05450517e2b5e13c33451e
short-sighted <helmhook@gmail.com>
2026-01-12 23:00:48 +0800

    fixed site-lisp/borg load-path

--- early-init.el
+++ early-init.el
@@ -1,7 +1,7 @@
 ;; -*- lexical-binding: t; no-byte-compile: t -*-
 (setq load-prefer-newer t)
 
-(add-to-list 'load-path (expand-file-name "site-lisp/borg"))
+(add-to-list 'load-path (expand-file-name "site-lisp/borg" user-emacs-directory))
 (require 'borg)
 (borg-initialize)
 
