bc296eabaf494b4c62d6f736194369d2e3a5c520
short-sighted <helmhook@gmail.com>
2026-01-15 16:51:32 +0800

    dream-ui changed

--- core/dream-ui.el
+++ core/dream-ui.el
@@ -10,5 +10,12 @@
   (:opt
    mood-line-glyph-alist mood-line-glyphs-fira-code))
 
+;; misc
+(setq inhibit-startup-screen t
+      inhibit-startup-echo-area-message user-login-name
+      inhibit-default-init t)
+(unless (daemonp)
+  (advice-add #'display-startup-echo-area-message :override #'ignore))
+
 (provide 'dream-ui)
 ;;; dream-ui.el ends here.
